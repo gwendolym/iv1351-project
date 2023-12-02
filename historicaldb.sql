44be823d93eeb8a6b8fa5fd8b2b4bae6d0f9c184
Hannah Veit <hannah.veit@gmx.de>
2023-12-02 19:49:05 +0100

    small change to first and last name

--- historicaldb.sql
+++ historicaldb.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE historical;
 
 CREATE TABLE lessons (
- lesson_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- lesson_type VARCHAR(50),
+ lesson_hist_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+ lesson_type VARCHAR(50) NOT NULL,
  genre VARCHAR(200),
  instrument VARCHAR(200),
  skill_level VARCHAR(50),
  lesson_price INT,
- student_name VARCHAR(200),
+ student_first_name VARCHAR(200),
+ student_last_name VARCHAR(200),
  student_email VARCHAR(200)
 );
