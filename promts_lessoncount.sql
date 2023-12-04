b6e2f5412f88aa840750f204104f7a8a75e46ede
Hannah Veit <hannah.veit@gmx.de>
2023-12-04 13:20:34 +0100

    changed naming

--- promts_lessoncount.sql
+++ promts_lessoncount.sql
@@ -1,13 +1,16 @@
 SELECT
-    EXTRACT(MONTH FROM l.start_time) AS part_norm,
-    COUNT(*) AS count_norm,
-    COUNT(DISTINCT il.lesson_id) AS count_indiv,
-    COUNT(DISTINCT e.lesson_id) AS count_ens,
-    COUNT(DISTINCT gl.lesson_id) AS count_gro
+    EXTRACT(MONTH FROM l.start_time) AS month,
+    COUNT(*) AS lessons,
+    COUNT(DISTINCT il.lesson_id) AS individual,
+    COUNT(DISTINCT e.lesson_id) AS ensembles,
+    COUNT(DISTINCT gl.lesson_id) AS groups
 FROM lesson l
-LEFT JOIN individual_lesson il ON l.lesson_id = il.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
-LEFT JOIN ensemble e ON l.lesson_id = e.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
-LEFT JOIN group_lesson gl ON l.lesson_id = gl.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
+LEFT JOIN individual_lesson il 
+ON l.lesson_id = il.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
+LEFT JOIN ensemble e 
+ON l.lesson_id = e.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
+LEFT JOIN group_lesson gl 
+ON l.lesson_id = gl.lesson_id AND EXTRACT(YEAR FROM l.start_time) = 2023
 WHERE EXTRACT(YEAR FROM l.start_time) = 2023
 GROUP BY EXTRACT(MONTH FROM l.start_time)
-ORDER BY part_norm ASC;
+ORDER BY month ASC;
