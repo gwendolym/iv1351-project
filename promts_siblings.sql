2835b5baf71fd85b362d8f3ebd1aea9414c4a4ee
Aspirant Aster <59698690+Sadisticsquirrel@users.noreply.github.com>
2023-12-04 15:07:14 +0100

    corrected and changes column names

--- promts_siblings.sql
+++ promts_siblings.sql
@@ -1,4 +1,4 @@
-SELECT count, COUNT(student_id) FROM (
+SELECT count - 1 AS no_of_siblings, COUNT(student_id) AS no_of_students FROM (
     SELECT student.student_id, t.count FROM student FULL JOIN (
         SELECT sibling_id, COUNT(student_id) FROM student GROUP BY sibling_id
     ) AS t on student.sibling_id = t.sibling_id
