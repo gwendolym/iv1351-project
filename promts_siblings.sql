979f2d20700f82c0fa05fe48e6c3df43e52619ac
Hannah Veit <hannah.veit@gmx.de>
2023-12-04 16:47:26 +0100

    removed explain

--- promts_siblings.sql
+++ promts_siblings.sql
@@ -1,4 +1,4 @@
-EXPLAIN ANALYZE Select count - 1 AS no_of_siblings, count(*) * count AS no_of_students 
+Select count - 1 AS no_of_siblings, count(*) * count AS no_of_students 
 FROM (
     SELECT sibling_id, count(student_id) 
     FROM student 
