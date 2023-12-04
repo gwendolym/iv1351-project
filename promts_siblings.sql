8b38f3155445c1a730e0c95f5bd1908c9b6be584
Hannah Veit <hannah.veit@gmx.de>
2023-12-04 16:47:07 +0100

    improved the query

--- promts_siblings.sql
+++ promts_siblings.sql
@@ -1,5 +1,6 @@
-SELECT count, COUNT(student_id) FROM (
-    SELECT student.student_id, t.count FROM student FULL JOIN (
-        SELECT sibling_id, COUNT(student_id) FROM student GROUP BY sibling_id
-    ) AS t on student.sibling_id = t.sibling_id
-) GROUP BY count;
+EXPLAIN ANALYZE Select count - 1 AS no_of_siblings, count(*) * count AS no_of_students 
+FROM (
+    SELECT sibling_id, count(student_id) 
+    FROM student 
+    GROUP BY sibling_id) 
+Group by count;
